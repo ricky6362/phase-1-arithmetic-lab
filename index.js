591ccc96cdd138332e471889e512f734355dbad0
ricky6362 <63843083+ricky6362@users.noreply.github.com>
2023-06-29 22:15:49 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+
+// multiplication generator
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+
+// Random generator
+const random = Math.floor(Math.random() * 10) + 1
+
+// mod generator
+const num3 = 30 
+const num4 = 13
+const mod = num3 % num4
+
+// Max generator
+let max = Math.max(20, 4, 7, 5, 9, 13, 17, 19)
